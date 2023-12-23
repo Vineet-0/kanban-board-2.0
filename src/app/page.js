5b4aa153ffafb66213e6eab836cbac97d0aa3b5d
Vineet-0 <vineet351016@gmail.com>
2023-12-23 23:50:45 +0530

    first commit 5

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,42 @@
 "use client"
 import { useState, useEffect } from 'react';
 
-import Navbar from './component/Navbar'
-import Body from './component/Body' 
+import Navbar from './component/Navbar';
+import Body from './component/Body';
 
 export default function Home() {
-  const [selectedGrouping, setSelectedGrouping] = useState('Status');
-  const [selectedOrdering, setSelectedOrdering] = useState('Priority');
-  
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setSelectedGrouping(localStorage.getItem('selectedGrouping'));
-      setSelectedOrdering(localStorage.getItem('selectedOrdering'));
-    }
-  }, []);
+  const [selectedGrouping, setSelectedGrouping] = useState(
+    typeof window !== 'undefined'
+      ? localStorage.getItem('selectedGrouping') || 'Status'
+      : 'Status'
+  );
+  const [selectedOrdering, setSelectedOrdering] = useState(
+    typeof window !== 'undefined'
+      ? localStorage.getItem('selectedOrdering') || 'Priority'
+      : 'Priority'
+  );
 
   useEffect(() => {
-    localStorage.setItem('selectedGrouping', selectedGrouping);
-    localStorage.setItem('selectedOrdering', selectedOrdering);
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('selectedGrouping', selectedGrouping);
+      localStorage.setItem('selectedOrdering', selectedOrdering);
+    }
   }, [selectedGrouping, selectedOrdering]);
 
   return (
     <div className='w-full min-h-screen bg-[#f4f5f8] dark:bg-[#02040a]'>
-      <Navbar selectedGrouping={selectedGrouping} setSelectedGrouping={setSelectedGrouping} selectedOrdering={selectedOrdering} setSelectedOrdering={setSelectedOrdering} />
-      <Body selectedGrouping={selectedGrouping} setSelectedGrouping={setSelectedGrouping} selectedOrdering={selectedOrdering} setSelectedOrdering={setSelectedOrdering} />
+      <Navbar
+        selectedGrouping={selectedGrouping}
+        setSelectedGrouping={setSelectedGrouping}
+        selectedOrdering={selectedOrdering}
+        setSelectedOrdering={setSelectedOrdering}
+      />
+      <Body
+        selectedGrouping={selectedGrouping}
+        setSelectedGrouping={setSelectedGrouping}
+        selectedOrdering={selectedOrdering}
+        setSelectedOrdering={setSelectedOrdering}
+      />
     </div>
-  )
+  );
 }
--- src/app/services/globalApi.jsx
+++ src/app/services/globalApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const boardBaseUrl="https://api.quicksell.co/v1/internal/frontend-assignment";
+const boardBaseUrl="https://tfyincvdrafxe7ut2ziwuhe5cm0xvsdu.lambda-url.ap-south-1.on.aws/ticketAndUsers";
 
 const GetBodyData = () => {
     return axios.get(boardBaseUrl);
